8f28aa8a388569bb484890bad0f508453588f126
Stefan <sdneamtu@gmail.com>
2018-01-11 22:42:33 -0500

    Added some simple console logging.
    Logging service is added so we can add more robust stuff later

---
+++ CryptoBot/Services/LoggingService.cs
@@ -0,0 +1,28 @@
+﻿using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace CryptoBot.Services
+{
+    public class LoggingService
+    {
+        private readonly DiscordSocketClient _client;
+        private readonly CommandService _commands;
+
+        public LoggingService(DiscordSocketClient client, CommandService commands)
+        {
+            _client = client;
+            _commands = commands;
+
+            _client.Log += LogAsync;
+        }
+
+        private Task LogAsync(LogMessage message)
+        {
+            return Console.Out.WriteLineAsync(message.ToString());
+        }
+    }
+}
