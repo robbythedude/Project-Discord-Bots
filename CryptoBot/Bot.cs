88367b8341a5fbad1a3d30cb04522e5dc05d7528
Stefan <sdneamtu@gmail.com>
2018-01-11 21:08:53 -0500

    Added a configuration file

--- CryptoBot/Bot.cs
+++ CryptoBot/Bot.cs
@@ -5,12 +5,14 @@ using Discord.WebSocket;
 using Discord.Commands;
 using Microsoft.Extensions.DependencyInjection;
 using System.Reflection;
+using Microsoft.Extensions.Configuration;
+
 
 namespace CryptoBot
 {
     public class Bot
     {
-        //initialize the main objects that the bot needs to be aware of in all methods
+        //initialize the main objects that the bot needs to be aware of in all methods.
         private DiscordSocketClient _client;
         private CommandService _commands;
         private IServiceProvider _services;
@@ -21,6 +23,8 @@ namespace CryptoBot
         /*the main method*/
         public async Task MainAsync()
         {
+
+            var config = GetConfiguration();
             //construct the "bot"
             _client = new DiscordSocketClient();
             _commands = new CommandService();
@@ -30,39 +34,52 @@ namespace CryptoBot
                         .BuildServiceProvider();
 
 
+            //add the commands we built to he service
             await ActivateCommands();
 
             //connect to discord
-            string token = "NDAxMTM1OTQyOTkzMzEzNzky.DTl-OA.yk8zSWgyRr0iwLiSI11VsMN7gJE";
+            string token = config["tokens:discord"];
             await _client.LoginAsync(TokenType.Bot, token);
             await _client.StartAsync();
 
             _client.Log += Log;
-            //_client.MessageReceived += MessageReceived;
-
-          
-
+            //_client.MessageReceived += MessageReceived
 
             //block this task until the program is closed
             await Task.Delay(-1);
         }
 
+        /*
+         * This method hooks into the MessageReceived Event and discovers all our commands and loads them
+         */
         public async Task ActivateCommands()
         {
+            //hook messages to our command handler
             _client.MessageReceived += HandleCommand;
+
+            //finds all our commands in this current assembly and loads them
             await _commands.AddModulesAsync(Assembly.GetEntryAssembly());
         }
 
+
+        /*
+         * Takes a message, checks if it is a command and then processes it
+         */
         private async Task HandleCommand(SocketMessage socketMessage)
         {
             var message = socketMessage as SocketUserMessage;
+
             if (message != null)
             {
+                //this is needed for the two methods used in the if statement. Just says where the prefix is located
                 int argPos = 0;
+
+                //using ! as the prefix
                 if (message.HasCharPrefix('!', ref argPos) || message.HasMentionPrefix(_client.CurrentUser, ref argPos))
                 {
                     var context = new SocketCommandContext(_client, message);
 
+                    //gets the result of the command after execution
                     var result = await _commands.ExecuteAsync(context, argPos, _services);
 
                     if (!result.IsSuccess)
@@ -71,22 +88,18 @@ namespace CryptoBot
                     }
                 }
             }
+
+            return;
         }
 
         /*
-        private async Task MessageReceived(SocketMessage message)
+         * Builds the configuration object
+         */
+        private IConfigurationRoot GetConfiguration()
         {
-            if (message.Content == "!ping")
-            {
-                await message.Channel.SendMessageAsync("Pong!");
-            }
 
-            else if (message.Content == "!BTC")
-            {
-                await message.Channel.SendMessageAsync("$0 Bitcoin is SHIT");
-            }
-        }*/
-
+            return new ConfigurationBuilder().SetBasePath(AppContext.BaseDirectory).AddJsonFile("appSettings.json").Build();
+        }
 
         /*
          * A method to handle Discord.Net's log events. Can use any logging framework here.
