4ccdc6eda216d0f8dc58261524c7df4aa853c0e9
Stefan <sdneamtu@gmail.com>
2018-01-11 20:33:42 -0500

    Refactored to use a better command structure

--- CryptoBot/Bot.cs
+++ CryptoBot/Bot.cs
@@ -2,33 +2,78 @@
 using System.Threading.Tasks;
 using Discord;
 using Discord.WebSocket;
+using Discord.Commands;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
 
 namespace CryptoBot
 {
     public class Bot
     {
+        //initialize the main objects that the bot needs to be aware of in all methods
+        private DiscordSocketClient _client;
+        private CommandService _commands;
+        private IServiceProvider _services;
+
         /*allows the program to immidiately start in async mode.*/
         public static void Main(string[] args) => new Bot().MainAsync().GetAwaiter().GetResult();
-
 
         /*the main method*/
         public async Task MainAsync()
         {
-            var client = new DiscordSocketClient();
+            //construct the "bot"
+            _client = new DiscordSocketClient();
+            _commands = new CommandService();
+            _services = new ServiceCollection()
+                        .AddSingleton(_client)
+                        .AddSingleton(_commands)
+                        .BuildServiceProvider();
 
-            client.Log += Log;
-            client.MessageReceived += MessageReceived;
+
+            await ActivateCommands();
 
             //connect to discord
-            string token = "";
-            await client.LoginAsync(TokenType.Bot, token);
-            await client.StartAsync();
+            string token = "NDAxMTM1OTQyOTkzMzEzNzky.DTl-OA.yk8zSWgyRr0iwLiSI11VsMN7gJE";
+            await _client.LoginAsync(TokenType.Bot, token);
+            await _client.StartAsync();
+
+            _client.Log += Log;
+            //_client.MessageReceived += MessageReceived;
+
+          
 
 
             //block this task until the program is closed
             await Task.Delay(-1);
         }
 
+        public async Task ActivateCommands()
+        {
+            _client.MessageReceived += HandleCommand;
+            await _commands.AddModulesAsync(Assembly.GetEntryAssembly());
+        }
+
+        private async Task HandleCommand(SocketMessage socketMessage)
+        {
+            var message = socketMessage as SocketUserMessage;
+            if (message != null)
+            {
+                int argPos = 0;
+                if (message.HasCharPrefix('!', ref argPos) || message.HasMentionPrefix(_client.CurrentUser, ref argPos))
+                {
+                    var context = new SocketCommandContext(_client, message);
+
+                    var result = await _commands.ExecuteAsync(context, argPos, _services);
+
+                    if (!result.IsSuccess)
+                    {
+                        await context.Channel.SendMessageAsync(result.ErrorReason);
+                    }
+                }
+            }
+        }
+
+        /*
         private async Task MessageReceived(SocketMessage message)
         {
             if (message.Content == "!ping")
@@ -40,7 +85,7 @@ namespace CryptoBot
             {
                 await message.Channel.SendMessageAsync("$0 Bitcoin is SHIT");
             }
-        }
+        }*/
 
 
         /*
