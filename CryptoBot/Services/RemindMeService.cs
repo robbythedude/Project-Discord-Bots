f6949e082cef695289073cfb5c52adba9bf1003f
Stefan <sdneamtu@gmail.com>
2018-01-18 21:57:44 -0500

    Who needs commits amiright

---
+++ CryptoBot/Services/RemindMeService.cs
@@ -0,0 +1,191 @@
+﻿using CryptoBot.Models;
+using CryptoBot.Modules;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace CryptoBot.Services
+{
+    public class RemindMeService
+    {
+        private static readonly string timeRegex = $"^(?!0)[0-9]+((year)|(month)|(day)|(hour)|(minute)|(second))";
+
+        public static string InputIntoDB(string[] stringArray, DateTime currentTime, string user)
+        {
+            string time = stringArray[0];
+            string message = stringArray[1];
+
+            if (Regex.IsMatch(time, timeRegex))
+            {
+                DateTime newtime = GetNewTime(time, currentTime);
+                 var reminder = new Reminder()
+                {
+                    User = user,
+                    Message = message,
+                    TimeToTrigger = newtime.ToString()
+                };
+
+                List<Reminder> reminders = ReadTextFile();
+                reminders.Add(reminder);
+                reminders = SortTextFile(reminders);
+                WriteToTextFile(reminders);
+            }
+            return "The time is not a valid format. Please use this format 12hours, 10minutes, 4seconds";
+        }
+
+        private static DateTime GetNewTime(string time, DateTime currentTime)
+        {
+            string numberRegex = $"[0-9]+";
+            int months = 0;
+            int hours = 0;
+            int minutes = 0;
+            int seconds = 0;
+
+            int number = Int32.Parse(Regex.Match(time, numberRegex).ToString());
+
+
+            if (time.Contains("month"))
+            {
+                months = number;
+            }
+            else if (time.Contains("hour")){
+                hours = number;
+            }
+            else if (time.Contains("minute")){
+                minutes = number;
+            }
+            else if (time.Contains("second"))
+            {
+                seconds = number;
+            }
+            DateTime newTime = currentTime.AddHours(hours).AddMinutes(minutes);
+
+            return newTime;
+        }
+
+        private static string GetTextFile()
+        {
+            string AssemblyPath = System.Reflection.Assembly.GetExecutingAssembly().Location;
+
+            string Directory = System.IO.Path.GetDirectoryName(AssemblyPath);
+            string fileName = "RemindMe.txt";
+            return Path.Combine(Directory, fileName);
+        }
+
+        private static void WriteToTextFile(List<Reminder> reminders)
+        {
+            string path = GetTextFile();
+            try
+            {
+                using (var file = new StreamWriter(path))
+                {
+                    foreach (Reminder reminder in reminders){
+                        string line = $"{reminder.TimeToTrigger}|{reminder.User}|{reminder.Message}";
+                        file.WriteLine(line);
+                    }
+                }
+            }
+            catch(Exception e)
+            {
+                throw e;
+            }
+
+        }
+
+        private static List<Reminder> ReadTextFile()
+        {
+            string path = GetTextFile();
+
+            var reminders = new List<Reminder>();
+            try
+            {
+                using (var file = new StreamReader(path))
+                {
+                    while (file.Peek() >= 0)
+                    {
+                        string line = file.ReadLine();
+
+                        string[] parsedLine = line.Split("|");
+
+                        if (parsedLine.Length != 3)
+                        {
+                            throw new Exception("There is something wrong with the text file");
+                        }
+                        var reminder = new Reminder()
+                        {
+                            TimeToTrigger = parsedLine[0],
+                            User = parsedLine[1],
+                            Message = parsedLine[2]
+                        };
+                        reminders.Add(reminder);
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+
+            return reminders;
+        }
+
+        public static Reminder GetFirstReminder()
+        {
+            string path = GetTextFile();
+
+            string firstLine = File.ReadLines(path).First();
+
+            if (!String.IsNullOrEmpty(firstLine))
+            {
+                string[] parsedLine = firstLine.Split("|");
+
+                var reminder = new Reminder()
+                {
+                    TimeToTrigger = parsedLine[0],
+                    User = parsedLine[1],
+                    Message = parsedLine[2]
+                };
+            }
+
+            return reminder;
+        }
+
+        public static void DeleteFirstReminder()
+        {
+            string path = GetTextFile();
+
+            var lines = File.ReadLines(path);
+            File.WriteAllLines(path, lines.Skip(1).ToArray());
+
+        }
+
+        private static List<Reminder> SortTextFile(List<Reminder> reminders)
+        {
+            reminders.Sort((x, y) => DateTime.Compare(Convert.ToDateTime(x.TimeToTrigger), Convert.ToDateTime(x.TimeToTrigger)));
+
+            return reminders;
+        }
+
+        public static string CheckTime()
+        {
+            string message = "";
+            Reminder reminder = GetFirstReminder();
+
+            DateTime currentTime = DateTime.UtcNow;
+
+            DateTime savedTime = Convert.ToDateTime(reminder.TimeToTrigger);
+
+            if (savedTime <= currentTime)
+            {
+                DeleteFirstReminder();
+                message = $"@{reminder.User} you are being reminded about {reminder.Message}";
+            }
+
+            return message;
+        }
+    }
+}
